189cb56f68e66385566d5d93f4b676b3cc378adc
Cidiomar Jr <cidiomar.jr@gmail.com>
2017-02-06 16:39:27 -0200

    clean admin.py imports

--- ovp_admin/admin.py
+++ ovp_admin/admin.py
@@ -1,27 +1,9 @@
 from django.contrib import admin
 
-#from django.core.exceptions import PermissionDenied
-#from django.http import HttpResponse, HttpResponseRedirect
-#from django.contrib.admin.util import lookup_field
-#from django.utils.html import strip_tags
-#from django.contrib import messages
-#from pyExcelerator import Workbook
-#from atados import settings
-#from django.utils.html import format_html
-
-#from django.core.urlresolvers import reverse
-
-
 import ovp_core.models as core
 import ovp_users.models as user
 import ovp_projects.models as project
 import ovp_organizations.models as organization
-
-
-#+- import ovp_uploads.models as upload
-#+- import ovp_projects.models as project
-#+- import ovp_search.models as search_models
-#+- import ovp_core.models as core
 
 
 adm_reg = admin.site._registry
@@ -42,30 +24,9 @@ if organization.Organization in adm_reg:
 	adm_reg[organization.Organization].display_on_main_menu = True
 
 
-
 try:
   import react_cms.models as react_cms
   if react_cms.ContentResource in adm_reg:
     adm_reg[react_cms.ContentResource].display_on_main_menu = True
 except ImportError:
   pass
-
-#++ ovp_projects.models.project.Project
-#++ ovp_projects.models.apply.apply
-#++ ovp_organizations.models.Organization
-#++ ovp_users.models.User
-
-
-
-#-- django.contrib.auth.models
-#-- email_log.models.Email
-
-#- ovp_core.models.skill.Skill
-#- ovp_core.models.cause.Cause
-#- ovp_core.models.address.GoogleAddress
-
-
-#+- ovp_projects.models.job.Job
-#+- ovp_projects.models.work.Work
-#+- ovp_projects.models.job.JobDate
-#+- ovp_uploads.models.UploadedImage
